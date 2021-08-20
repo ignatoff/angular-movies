0f8da2df65fca54afce10d4ce3280c04c2caba86
ignatoff <ign.nikolay@gmail.com>
2021-08-20 17:17:11 +0300

    small updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,27 @@ import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesModule } from 'src/app/movies/movies.module';
+import { CoreModule } from 'src/app/core/core.module';
+import { MaterialModule } from 'src/app/material.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
+    MoviesModule,
+    MaterialModule,
+
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
