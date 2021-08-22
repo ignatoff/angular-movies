c4230bab3a4c7821b189dfdb57e041c7273c72c6
ignatoff <ign.nikolay@gmail.com>
2021-08-22 10:19:36 +0300

    layout changes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesListComponent } from 'src/app/movies/movies-list/movies-list.component';
+import { HomeComponent } from 'src/app/shared/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'movies',
+    component: MoviesListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
