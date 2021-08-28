6e47a353b26a5df779895c40f4cdfd7161ec79d5
ignatoff <ign.nikolay@gmail.com>
2021-08-28 10:04:22 +0300

    IC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +34,6 @@ import { AuthModule } from 'src/app/auth/auth.module';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     FlexLayoutModule,
-    BrowserAnimationsModule,
     
     AppRoutingModule,
   ],
